cc1988e1d93adcec43b53dcb9d8ca398f00dad50
natanvictors <natanvicro@gmail.com>
2025-11-26 18:48:41 -0300

    Finalizado

--- TrabalhoGB.java
+++ TrabalhoGB.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Tudojunto {
+public class TrabalhoGB {
 
     // FINANCEIRO GLOBAL
     static double totalMotosFinanceiro = 0;
